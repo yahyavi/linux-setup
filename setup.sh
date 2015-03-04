b91e3a23d0e1080126c70f33766976b135fa6a7c
saman <nebeleh@gmail.com>
2015-03-04 18:18:34 -0500

    updated node to version 0.12

--- setup.sh
+++ setup.sh
@@ -15,8 +15,8 @@ bash install.sh
 
 # Load nvm and install latest production node
 source $HOME/.nvm/nvm.sh
-nvm install v0.10
-nvm use v0.10
+nvm install v0.12
+nvm use v0.12
 
 # Install rlwrap to provide libreadline features with node
 # See: http://nodejs.org/api/repl.html#repl_repl
